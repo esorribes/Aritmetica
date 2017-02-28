6001c9f1b00e0e4d6232b08870dcada665e49400
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2017-02-28 16:29:08 +0100

    Limpiando de comentarios los tests.

--- src/test/java/aritmetica/test/AritmeticaTest.java
+++ src/test/java/aritmetica/test/AritmeticaTest.java
@@ -29,27 +29,22 @@ public class AritmeticaTest {
 
 	@Test
 	public void testSuma() {
-//		Aritmetica aritmetica = new Aritmetica();
 		assertThat(aritmetica, notNullValue());
 		assertThat(aritmetica.suma(2, 3), is(5.0f));
-//		assertEquals(2, aritmetica.suma(1, 1), 0);
 	}
 
 	@Test
 	public void testResta() {
-//		Aritmetica aritmetica = new Aritmetica();
 		assertEquals(3, aritmetica.resta(4, 1), 0);
 	}
 
 	@Test
 	public void testMultiplicacion() {
-//		Aritmetica aritmetica = new Aritmetica();
 		assertEquals(6, aritmetica.multiplicacion(2, 3), 0);
 	}
 
 	@Test
 	public void testDivision() {
-//		Aritmetica aritmetica = new Aritmetica();
 		assertEquals(5, aritmetica.division(10, 2), 0);
 	}
 
